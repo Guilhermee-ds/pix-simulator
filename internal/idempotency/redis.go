f4331e1263f323c332170aee3fa6757b2cc0ce6a
Guilherme Oliveira <guilhermesantos7334@gmail.com>
2026-03-28 20:58:12 -0300

    Feat: Add idempotency in redis for project

--- internal/idempotency/redis.go
+++ internal/idempotency/redis.go
@@ -1,1 +1,19 @@
 package idempotency
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func Exists(rdb *redis.Client, key string) bool {
+	val, _ := rdb.Exists(ctx, key).Result()
+	return val == 1
+}
+
+func Save(rdb *redis.Client, key string) {
+	rdb.Set(ctx, key, "1", time.Minute*10)
+}
