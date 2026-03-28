694e26278bc30235d4daab9ef8af7dc3326a7c7b
Guilherme Oliveira <guilhermesantos7334@gmail.com>
2026-03-28 20:40:27 -0300

    Feat: Add redis queue for project

--- internal/queue/redis.go
+++ internal/queue/redis.go
@@ -1,1 +1,30 @@
 package queue
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+type Job struct {
+	ID string
+}
+
+func Push(rdb *redis.Client, key string, job Job) {
+	data, _ := json.Marshal(job)
+	rdb.LPush(ctx, key, data)
+}
+
+func Pop(rdb *redis.Client, key string) (Job, error) {
+	val, err := rdb.RPop(ctx, key).Result()
+	if err != nil {
+		return Job{}, err
+	}
+
+	var job Job
+	json.Unmarshal([]byte(val), &job)
+	return job, nil
+}
