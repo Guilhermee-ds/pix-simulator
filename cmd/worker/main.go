be79971276d1a0202e559180659c97a5322212b2
Guilherme Oliveira <guilhermesantos7334@gmail.com>
2026-03-28 21:09:21 -0300

    Feat: add worker main

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,39 @@
 package worker
+
+import (
+	"fmt"
+	"pix-simulator/internal/database"
+	"pix-simulator/internal/queue"
+	"pix-simulator/internal/service"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func worker(rdb *redis.Client, svc *service.Service) {
+	for {
+		job, err := queue.Pop(rdb, "pix")
+		if err != nil {
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+
+		err = svc.Process(job.ID)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+	}
+}
+
+func main() {
+	db := database.Connet()
+	svc := service.New(db)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+
+	for i := 0; i < 200; i++ {
+		go worker(rdb, svc)
+	}
+}
